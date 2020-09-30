a53c75c726c9217d85db8252d3c7baecc997c7d0
Ludvig Lindau <ludviglindau@gmail.com>
2020-09-30 13:39:06 +0200

    Set up makefile and dir structure

--- db_server/test.sh
+++ db_server/test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib
-./demo "CREATE TABLE students (id INT, first_name VARCHAR(8), last_name VARCHAR(8));"
-./demo ".tables"
-./demo ".schema students"
-./demo "DROP TABLE students;"
-./demo "INSERT INTO students VALUES (42, 'Oscar', 'Svensson');"
-./demo "SELECT * FROM students;"
-./demo "CREATE TABLE students (id INT, first_name VARCHAR(7), last_name VARCHAR(8), PRIMARY KEY(id));"
-./demo "SELECT first_name, last_name FROM students;"
-./demo "DELETE FROM students WHERE id=42;"
-./demo "UPDATE students SET first_name='Emil', last_name='Johansson' WHERE id=42;"
+./db_server "CREATE TABLE students (id INT, first_name VARCHAR(8), last_name VARCHAR(8));"
+./db_server ".tables"
+./db_server ".schema students"
+./db_server "DROP TABLE students;"
+./db_server "INSERT INTO students VALUES (42, 'Oscar', 'Svensson');"
+./db_server "SELECT * FROM students;"
+./db_server "CREATE TABLE students (id INT, first_name VARCHAR(7), last_name VARCHAR(8), PRIMARY KEY(id));"
+./db_server "SELECT first_name, last_name FROM students;"
+./db_server "DELETE FROM students WHERE id=42;"
+./db_server "UPDATE students SET first_name='Emil', last_name='Johansson' WHERE id=42;"
